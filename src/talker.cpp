894ce1df02bc1bc5cbadb88e2c5f99930e94bdc3
Learner1729 <ashishpatel0894@gmail.com>
2018-10-30 23:51:28 -0400

    Performed static code analysis using cppcheck and cpplint - Output generated is stored in a results directory - talker.cpp file was modified to remove whitespace cpplint error

--- src/talker.cpp
+++ src/talker.cpp
@@ -109,7 +109,7 @@ int main(int argc, char **argv) {
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
-    
+
     /**
      * The publish() function is how you send messages. The parameter
      * is the message object. The type of this object must agree with the type
